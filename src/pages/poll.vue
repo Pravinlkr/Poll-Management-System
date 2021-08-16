4e1a62d829182f85fff372afa9854e9d3824761d
Pravinlkr <pravinlakhisarai@gmail.com>
2021-08-16 19:09:50 +0530

    implemented add, edit and delete poll feature

--- src/pages/poll.vue
+++ src/pages/poll.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="mt-3">
-  <h3>All Active Polls</h3>
+      <div class="headerOfPoll">
+          <h3>All Active Polls</h3>
+          <add-polls></add-polls>
+      </div>
   <b-container class="bv-example-row">
   <b-row>
     <b-col v-for="(polls, index) in getPolls" v-bind:key="index">
       <b-card style="width: 20rem;" class="mb-2">
-        <b-card-title>{{polls.title}}</b-card-title>
+        <b-card-title>
+            <span>{{polls.title}}</span>        
+        </b-card-title>
         <b-card-text v-for="(option, index) in polls.options" v-bind:key="index">
           {{option.option}}
         </b-card-text>
         <b-card-footer>
-                <b-button variant="outline-primary" size="sm">Edit</b-button>
-                <b-button variant="outline-danger" size="sm" class="ml-3">Delete</b-button>
+                <edit-poll :editId="polls._id"></edit-poll>
+                <b-button variant="outline-danger" size="sm" class="ml-3" @click="deleteSinglePoll({id:polls._id})">Delete</b-button>
         </b-card-footer>
       </b-card>
     </b-col>
@@ -23,14 +28,23 @@
 </template>
 
 <script>
+import editPoll from '../components/editPoll.vue'
+import addPolls from '../components/addPolls.vue'
 import {mapActions, mapGetters} from 'vuex'
 export default {
     name:'Poll',
+    components:{
+        addPolls,
+        editPoll
+    },
     computed:{
       ...mapGetters('poll',['getPolls'])
     },
     methods:{
-      ...mapActions('poll',['getAllPolls'])
+      ...mapActions('poll',['getAllPolls','deletePoll']),
+      deleteSinglePoll(payload){
+          this.deletePoll(payload);
+      }
     },
     mounted(){
       this.getAllPolls();
--- src/store/modules/poll.module.js
+++ src/store/modules/poll.module.js
@@ -3,11 +3,15 @@ const state = {
     loggedIN:false,
     allPolls:[],
     role: '',
-    message:''
+    message:'',
+    selectedPoll:[]
 }
 const getters = {
     getPolls(state){
         return state.allPolls;
+    },
+    getSelectedPoll(state){
+        return state.selectedPoll;
     },
     getMessage(state){
         return state.message;
@@ -43,6 +47,24 @@ const mutations = {
     LOGOUT(state){
         state.loggedIN = false
         localStorage.clear();
+    },
+    ADD_POLL(state){
+        state.message = 'New Payload added'
+    },
+    FETCH_POLL_WITH_ID(state,payload){
+        state.selectedPoll = payload
+    },
+    REMOVE_OPTION(state){
+        state.message = 'option removed'
+    },
+    UPDATE_POLL_TITLE(state){
+        state.message = 'poll title updated'
+    },
+    ADD_NEW_OPTION(state){
+        state.message = 'new Options Added'
+    },
+    DELETE_POLL(state,payload){
+        state.message = payload.id + 'Id no poll is deleted'
     }
 }
 const actions = {
@@ -81,6 +103,79 @@ const actions = {
     },
     logOut({commit}){
         commit('LOGOUT');
+    },
+    addPoll({commit, dispatch},payload){
+        axios.get(`https://secure-refuge-14993.herokuapp.com/add_poll?title=${payload.title}&options=${payload.allOptions}`)
+        .then(response=>{
+            if(response.data.error == 0){
+                dispatch('getAllPolls')
+            }
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+        commit('ADD_POLL',payload)
+    },
+    fetchPollWithId({commit},payload){
+        if(payload){
+        axios.get(`https://secure-refuge-14993.herokuapp.com/list_poll?id=${payload}`)
+        .then(response=>{
+            commit('FETCH_POLL_WITH_ID',response.data.data)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
+    },
+    removeOption({commit, dispatch},payload){
+        axios.get(`https://secure-refuge-14993.herokuapp.com/delete_poll_option?id=${payload.id}&option_text=${payload.optionText}`)
+        .then(response=>{
+            console.log(response)
+            commit('REMOVE_OPTION')
+            dispatch('fetchPollWithId',payload.id)
+            dispatch('getAllPolls')
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    },
+    updatePollTitle({commit, dispatch},payload){
+        axios.get(`https://secure-refuge-14993.herokuapp.com/update_poll_title?id=${payload.id}&title=${payload.title}`)
+        .then(response=>{
+            console.log(response)
+            commit('UPDATE_POLL_TITLE');
+            dispatch('getAllPolls')
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    },
+    addNewOptionAction({commit,dispatch},payload){
+        for(let i=0; i<payload.data.length; i++){
+            var opt = payload.data[i].value;
+            console.log(opt)
+            axios.get(`https://secure-refuge-14993.herokuapp.com/add_new_option?id=${payload.id}&option_text=${opt}`)
+            .then(response=>{
+                console.log(response)
+                commit('ADD_NEW_OPTION')
+                dispatch('getAllPolls')
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+        }
+    },
+    deletePoll({commit, dispatch},payload){
+        axios.get(`https://secure-refuge-14993.herokuapp.com/delete_poll?id=${payload.id}`)
+        .then(response=>{
+            if(response.data.error == 0){
+                dispatch('getAllPolls')
+            }
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+        commit('DELETE_POLL', payload)
     }
 }
 
